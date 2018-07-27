eaf469bac485fc6834ffe6eed19e67bca681dba0
Ad1n <shevtsovav@bk.ru>
2018-07-28 01:12:01 +0300

    Lesson 3. Corrections. Add references  to tables. Add null-false

--- db/migrate/20180727125747_add_defaults_values_level_and_correct.rb
+++ db/migrate/20180727125747_add_defaults_values_level_and_correct.rb
@@ -2,5 +2,7 @@ class AddDefaultsValuesLevelAndCorrect < ActiveRecord::Migration[5.1]
   def change
     change_column :answers, :correct, :boolean, default: true
     change_column :tests, :level, :integer, default: 0
+    # change_column_default(:answers, :correct, true)
+    # change_column_default(:tests, :level, 0)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180727125747) do
+ActiveRecord::Schema.define(version: 20180727220738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 20180727125747) do
     t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "correct", default: true
+    t.boolean "correct", default: true, null: false
+    t.bigint "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -32,6 +34,8 @@ ActiveRecord::Schema.define(version: 20180727125747) do
     t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "test_id"
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -39,6 +43,8 @@ ActiveRecord::Schema.define(version: 20180727125747) do
     t.integer "level", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +55,7 @@ ActiveRecord::Schema.define(version: 20180727125747) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
