4154600293d71fc19db6d9946ad4cc880ccd02f5
Ad1n <shevtsovav@bk.ru>
2018-08-10 18:37:35 +0300

    Lesson 7. Corrections. change form_with

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :select_test, only: %i[index create]
+  before_action :select_test, only: %i[index create new]
   before_action :select_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -14,7 +14,7 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
+    @question = @test.questions.new
   end
 
   def create
