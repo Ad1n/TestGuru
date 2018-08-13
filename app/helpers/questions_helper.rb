396ea282f2a0a3f6e9b38c31f8940d5a4b0f89ad
Ad1n <shevtsovav@bk.ru>
2018-08-13 13:30:56 +0300

    Lesson 8. Corrections. Change question_header. Add attributes.

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,9 +1,8 @@
 module QuestionsHelper
 
-  def question_header
-    question = Question.find(params[:id]) if action_name == 'edit'
-    test = Test.find(params[:test_id]) if action_name == 'new'
-    question.nil? ? "Create #{test.title} question" :
-                    "Edit #{question.test.title} question"
+  def question_header(test, question)
+    question.persisted? ? "Edit #{question.test.title} question" :
+                          "Create #{test.title} question"
+
   end
 end
