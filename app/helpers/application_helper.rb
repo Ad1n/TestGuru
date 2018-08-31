e36db3adff57ef5205c452c56a56fcb7281a1a4e
Ad1n <shevtsovav@bk.ru>
2018-08-31 13:24:11 +0700

    Lesson 13. Add bootstrap to TestGuru project.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'GitHub', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def admin?
+    current_user.is_a?(Admin) && !current_user.nil?
+  end
 end
