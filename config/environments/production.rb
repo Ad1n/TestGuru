c19422d5cb988b8d57cc853cd163e13850424be7
Ad1n <shevtsovav@bk.ru>
2018-09-19 15:42:07 +0300

    Lesson 17. Configure action mailer defaul URL for correct confirmations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://become-guru.herokuapp.com/gurus/login' }
+  config.action_mailer.default_url_options = { host: 'https://become-guru.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
